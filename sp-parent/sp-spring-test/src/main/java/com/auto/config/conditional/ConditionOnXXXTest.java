0c6bd54d397623a8cedfd5aadaf9d662c94db072
liyaqiu <361234567@qq.com>
2022-05-10 17:35:20 +0800

    2022-5-10

--- sp-parent/sp-spring-test/src/main/java/com/auto/config/conditional/ConditionOnXXXTest.java
+++ sp-parent/sp-spring-test/src/main/java/com/auto/config/conditional/ConditionOnXXXTest.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
 /**
--- sp-parent/sp-spring-test/src/main/java/com/controller/TestController.java
+++ sp-parent/sp-spring-test/src/main/java/com/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
