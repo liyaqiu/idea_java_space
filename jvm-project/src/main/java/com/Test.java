952cadae1e81dbcdb1b265246ca5d4b8ffbb6873
liyaqiu <361234567@qq.com>
2022-05-15 18:10:16 +0800

    2022-5-15

--- jvm-project/src/main/java/com/Test.java
+++ jvm-project/src/main/java/com/Test.java
@@ -5,8 +5,12 @@ package com;
  * @date 2022/5/13 11:51
  **/
 public class Test {
-    static int i = 1;
-    public static void main(String[] args) throws InterruptedException {
-
-    }
+   public void test(){
+       int c = 1000;
+       int a = test1();
+       int b = 100;
+   }
+   public int test1(){
+       return 1000;
+   }
 }
