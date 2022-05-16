f952bd1bec16e26ce661273f67f8ec25c6c5f97c
liyaqiu <361234567@qq.com>
2022-05-16 18:50:35 +0800

    2022-5-16

--- jvm-project/src/main/java/com/Test.java
+++ jvm-project/src/main/java/com/Test.java
@@ -5,13 +5,7 @@ package com;
  * @date 2022/5/13 11:51
  **/
 public class Test {
-   public void test(){
-       int c = 1000;
-       int a = test1();
-       int b = 100;
-   }
-   public int test1(){
-       int a = 100;
-       return a;
+   public static void main(String[] args) throws InterruptedException {
+      Thread.sleep(500000);
    }
 }
