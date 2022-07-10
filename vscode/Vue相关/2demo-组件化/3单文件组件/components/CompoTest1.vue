45157649a5220fd81830da97acf18c65db0811b5
liyaqiu <361234567@qq.com>
2022-07-10 19:43:25 +0800

    2022-7-10

---
+++ vscode/Vue相关/2demo-组件化/3单文件组件/components/CompoTest1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button id="bt">{{msg}}</button>
+  </div>
+</template>
+
+<script>
+    //export Vue.extend({})
+    export default {
+      name:'CompoTest1',
+      data(){
+        return {
+          msg: '我是模块1'
+        }
+      }
+    }
+</script>
+
+<style>
+  #bt{
+    border: 1px yellow;
+  }
+</style>
---
+++ vscode/Vue相关/2demo-组件化/3单文件组件/components/CompoTest2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button id="bt">{{msg}}</button>
+  </div>
+</template>
+
+<script>
+    //export Vue.extend({})
+    export default {
+      name:'CompoTest2',
+      data(){
+        return {
+          msg: '我是模块2'
+        }
+      }
+    }
+</script>
+
+<style>
+  #bt{
+    border: 1px seagreen;
+  }
+</style>
