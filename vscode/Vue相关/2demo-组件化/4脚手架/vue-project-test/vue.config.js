82c19901a96328f2d1cd9cb1a0026bb91ada390f
liyaqiu <361234567@qq.com>
2022-07-11 01:13:58 +0800

    2022-7-11

--- vscode/Vue相关/2demo-组件化/4脚手架/vue-project-test/vue.config.js
+++ vscode/Vue相关/2demo-组件化/4脚手架/vue-project-test/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false //关闭语法检查
+  lintOnSave: false, //关闭语法检查
+  pages: {
+    index: {
+      // page 的入口
+      entry: '2src-元素的ref属性/main.js'
+    }
+  }
 })
